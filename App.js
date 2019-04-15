ece133c777498d486877007c25bea6771161a3cc
Manoj <manoj.delhi24@gmail.com>
2019-04-15 08:26:37 +0700

    Adding basic styling to header component.

--- App.js
+++ App.js
@@ -11,7 +11,8 @@ import Header from './src/components/header';
 
 type Props = {};
 export default class App extends Component<Props> {
+
   render() {
-    return <Header />;
+    return <Header text={'Albums'} />;
   }
 }
