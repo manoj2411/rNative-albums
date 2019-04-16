d712b53158bcd637b2e10d786c1be4de3feb4c74
Manoj <manoj.delhi24@gmail.com>
2019-04-16 18:06:50 +0700

    Creating Card and Card Section layout for Albums list.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import axios from 'axios';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends React.Component {
+  state = { albums: [] };
 
   componentWillMount() {
-    axios.get('http://reduxblog.herokuapp.com/api/posts?key=123')
+    axios.get('http://rallycoding.herokuapp.com/api/music_albums')
           .then((response) => {
-            console.log(response);
+            this.setState({ albums: response.data });
           });
+  }
+
+  renderAlbumDetails() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
   }
 
   render() {
     return (
-        <View style={{ alignItems: 'center' }}>
-          <Text>Album list </Text>
+        <View>
+          {this.renderAlbumDetails()}
         </View>
       );
   }
 }
+
 
 export default AlbumList;
