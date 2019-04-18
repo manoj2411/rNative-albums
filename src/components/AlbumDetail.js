7f6feec011843f47003d04c6d44940367ee2df66
Manoj <manoj.delhi24@gmail.com>
2019-04-18 09:01:58 +0700

    Adding Button component with basic button styling.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => (
   <Card>
@@ -22,6 +23,9 @@ const AlbumDetail = ({ album }) => (
         style={styles.imageStyle}
         source={{ uri: album.image }}
       />
+    </CardSection>
+    <CardSection>
+      <Button text="Button 1" />
     </CardSection>
   </Card>
 );
